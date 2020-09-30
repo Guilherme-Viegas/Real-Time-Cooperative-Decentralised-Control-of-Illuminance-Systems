97af3f84b100b49b4dfbf67fdc37d0301067f862
Guilherme Viegas <ggmviegas@gmail.com>
2020-09-30 22:03:50 +0100

    Add files via upload

--- Labs_Viegas/lab1.ino
+++ Labs_Viegas/lab1.ino
@@ -13,24 +13,30 @@ void loop() {
   readLdr();
   if(Serial.available() > 0) {
       inBytes = Serial.readString();
-      Serial.print("I received: "); Serial.println(inBytes);
+      Serial.print("I received: ");
+      Serial.println(inBytes);
       analogWrite(10, map(inBytes.toInt(), 0, 100, 0, 255));
-      delay(2);
     }
+    delay(10);
 }
 
 void readLdr() {
     // read the input on analog pin 0:
     int sensorValue = analogRead(A0);
     // Convert the analog reading (which goes from 0 - 1023) to a percentage (0-100)
-    float voltage = sensorValue * (100.0 / 1023.0);
+    float voltageOut = sensorValue * (5.0 / 1023.0);
     // print out the value you read:
-    Serial.println(String(voltage));
+    Serial.print("LUX VALUE: ");
+    Serial.println(String(voltageToLux(voltageOut)));
   }
 
-int voltageToLux(int v0) {
-    int vcc = 5;
-    int R1 = 10000;
-    int m = -1;//slope
-    int b = 32; //Intersection
+float voltageToLux(float v0) {
+    float vcc = 5.0;
+    float R1 = 10000.0;
+    float m = -1.7780;//slope
+    float b = 4.778; //Intersection = log10(60000)kOhm
+
+    float function = (log10((vcc/v0)*R1 - R1) - b) / m;
+    float lux = pow(10, function);
+    return lux;
   }
