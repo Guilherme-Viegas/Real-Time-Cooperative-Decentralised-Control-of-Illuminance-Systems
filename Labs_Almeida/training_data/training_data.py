9de185f27fab6cfec2da18ccf0ff08a2025f8fc6
João Almeida <joao.marques.almeida@tecnico.ulisboa.pt>
2020-10-12 18:42:46 +0100

    nice plot

--- Labs_Almeida/training_data/training_data.py
+++ Labs_Almeida/training_data/training_data.py
@@ -10,7 +10,7 @@ def computeBeta(x: np.array, y: np.array) -> np.array:
     return inv(x.T@x)@x.T@y # compute beta
 
 # load variables from files and check their dimensions
-def load_variables(name_file_x: str,) -> (np.array,np.array):
+def load_variables(name_file_x: str) -> (np.array,np.array):
 
     # load files
     f = codecs.open(name_file_x, "r", "utf-16")
@@ -19,16 +19,22 @@ def load_variables(name_file_x: str,) -> (np.array,np.array):
     temp = temp.rsplit('\r\n')  # slip each line
     temp.pop() # remove last line
 
-    lux_ = []
+    lux = []
     R2 = []
 
     for line in temp:
         s = line.rsplit('\t')
-        lux_.append(s[0])
+        lux.append(s[0])
         R2.append(s[1])
 
-    lux_ = np.array(lux_, dtype=np.float64)
+    lux = np.array(lux, dtype=np.float64)
     R2 = np.array(R2, dtype=np.float64)
+
+    return (lux,R2)
+
+
+def format_variables(R2: np.array, lux_: np.array) -> (np.array,np.array):
+
     R2 = np.log10(R2)
 
     N = max(R2.shape)    # number of polynomials
@@ -38,10 +44,10 @@ def load_variables(name_file_x: str,) -> (np.array,np.array):
     return (lux,R2)
 
 
-
 if TRAINING:
 
     x,y = load_variables('training_data3.txt')
+    x,y = format_variables(x,y)
     beta = computeBeta(x,y)
     fit = x@beta
     print(f"Data size: {max(y.shape)}")
@@ -51,5 +57,5 @@ if TRAINING:
 else:
 
     x,y = load_variables('results2.txt')
-    plt.scatter(x[:,1],y, lw=1)
+    plt.scatter(x,y, s=1)
     plt.show()
